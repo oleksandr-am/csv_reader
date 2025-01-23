aa4a50171b1b7be8fb8a86462398b878963edd85
Oleksandr Kryvoshapka <oleksandr@atlasmetrics.io>
2025-01-23 18:21:16 +0100

    Initial implementation (unoptimized)

---
+++ README.md
@@ -0,0 +1,57 @@
+# Engineering Challenge 2024 - Colocation
+
+Welcome to the Engineering Challenge 2024 - Colocation repository!
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+This repository contains the code and documentation for the Engineering Challenge 2024 focused on colocation.
+
+## Requirements
+
+- Rust
+- Git
+
+## Installation
+
+1. Clone the repository:
+    ```bash
+    git clone https://github.com/yourusername/engineering-challenge-2024-colocation.git
+    ```
+2. Navigate to the project directory:
+    ```bash
+    cd engineering-challenge-2024-colocation/csv_reader
+    ```
+3. Place your intput file inside /data directory:
+    ```bash
+    ./data/weather_stations.csv
+    ```
+
+## Usage
+
+To run the project, execute the following command:
+```bash
+cd engineering-challenge-2024-colocation/csv_reader
+cargo run
+```
+
+The results can be found in /data folder:
+```bash
+./data/outputs.txt
+```
+
+## Contributing
+
+We welcome contributions! Please read our [CONTRIBUTING.md](CONTRIBUTING.md) file for guidelines on how to contribute to this project.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
---
+++ src/main.rs
@@ -0,0 +1,79 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use std::collections::HashMap;
+
+fn read_csv<P: AsRef<Path>>(filename: P) -> Result<(), Box<dyn Error>> {
+    let file = File::open(filename)?;
+    let mut rdr = csv::Reader::from_reader(file);
+
+    let mut obj = HashMap::new();
+   
+    for result in rdr.records() {
+        let record = result?;
+
+        let string_part = record.get(0).unwrap().to_string(); 
+        let parts: Vec<&str> = string_part.split(';').collect();
+
+        let city_name = parts[0].to_string();
+        let temp_value: f64 = parts[1].parse().expect("Failed to parse number");
+
+        obj.entry(city_name)
+            .or_insert_with(Vec::new)
+            .push(temp_value);
+        
+        // TODO: read line and store intermediate results in a HashMap
+    }
+
+    // Collect the entries into a vector and sort by key
+    let mut sorted_entries: Vec<_> = obj.iter().collect();
+    sorted_entries.sort_by(|a, b| a.0.cmp(b.0));
+
+    // Create the output string
+    let mut output = String::new();
+    output.push('{');
+
+    for (i, (city, temps)) in sorted_entries.iter().enumerate() {
+        let min_value = temps.iter().cloned().reduce(f64::min).unwrap();
+        let max_value = temps.iter().cloned().reduce(f64::max).unwrap();
+        let sum: f64 = temps.iter().sum();
+        let avg_value = sum / temps.len() as f64;
+
+        if i > 0 {
+            output.push_str(", ");
+        }
+        output.push_str(&format!("{}={:.1}/{:.1}/{:.1}", city, min_value, avg_value, max_value));
+    }
+
+    output.push('}');
+    // println!("{}", output);
+
+    // Write the output to a file
+    let mut file = File::create("../data/output.txt")?;
+    file.write_all(output.as_bytes())?;
+ 
+    // println!("{}", output);
+
+    // Print the final HashMap to verify
+    // for (city, temps) in &obj {
+    //     let minValue = temps.iter().cloned().reduce(f64::min);
+    //     let maxValue = temps.iter().cloned().reduce(f64::max);
+    //     let sum: f64 = temps.iter().sum::<f64>();
+    //     let avgValue = sum / temps.len() as f64;
+    //     println!("City: {}, Temperatures: {:?},  Number of records: {:?}, Min: {:?}, Max: {:?}, Average: {:?}",
+    //     city, 
+    //     temps, 
+    //     temps.len(),
+    //     minValue,
+    //     maxValue,
+    //     avgValue);
+    // }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let filename = "../data/weather_stations.csv";
+    read_csv(filename)
+}
